82e9db7ba24b1cd43df8809b9bda8e8ca237ffa2
syrnik1 <95712566+syrnik1@users.noreply.github.com>
2022-02-02 21:35:51 +0300

    update2

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
   let arr = str.split('');
-  console.log(arr)
   for (let i = 0; i < bracketsConfig.length; i++) {
-    let x = i;
+    let x = bracketsConfig[i];   
     for (let j = 0; j < arr.length; j++) {
-      if (arr[j] === bracketsConfig[x][0] && arr[j+1] === bracketsConfig[x][1]) {
+      if (arr[j] == x[0] && arr[j+1] == x[1]) {
         arr.splice(j,2);
-        j = j - 2;
-        i = - 1;
-      }
-    }           
-  }
-  if(arr.length === 0) {
-      return true;
-    } else {
-      creturn false;
-    }
+        j -= 2;
+        i = -1;
+        if(!arr.length) {
+          return true;
+        };
+      };
+    };
+  };
+  return false;
 }
