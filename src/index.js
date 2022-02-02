9ad35ac5682eea5b61a4ec493c1798f46643c60d
syrnik1 <95712566+syrnik1@users.noreply.github.com>
2022-02-02 21:29:58 +0300

    update 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = str.split('');
+  console.log(arr)
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let x = i;
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] === bracketsConfig[x][0] && arr[j+1] === bracketsConfig[x][1]) {
+        arr.splice(j,2);
+        j = j - 2;
+        i = - 1;
+      }
+    }           
+  }
+  if(arr.length === 0) {
+      return true;
+    } else {
+      creturn false;
+    }
 }
